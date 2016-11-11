18a4afbc08eab1f89de9d4559c43c81655946afd
Edwin <mardwintrader@gmail.com>
2016-11-11 18:05:38 -0400

    actualización
    Actualización

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var page = require('page');
+var main = document.getElementById('main-container')
+page('/', function (ctx, next) {
+	main.innerHTML = 'Home <a href="/signup">Signup</a>';
+})
+
+page('/signup', function (ctx, next) {
+	main.innerHTML = 'Signup <a href="/">Home</a>';
+})
+page();
