1f71f8fc810177ed4d309bf197e0dec551be1fa6
Edwin <mardwintrader@gmail.com>
2016-11-11 19:22:51 -0400

    Commit 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 var page = require('page');
 var main = document.getElementById('main-container')
 page('/', function (ctx, next) {
-	main.innerHTML = 'Home <a href="/signup">Signup</a>';
+	main.innerHTML = 'Home <a href="/signup">Signup 2</a>';
 })
 
 page('/signup', function (ctx, next) {
-	main.innerHTML = 'Signup <a href="/">Home</a>';
+	main.innerHTML = 'Signup <a href="/">Home 14</a>';
 })
 page();
