78bcae4da7db39fef8ec09896ee7d0f39f1193ef
Edwin <mardwintrader@gmail.com>
2016-11-14 12:06:43 -0400

    Commit 6

--- src/index.js
+++ src/index.js
@@ -1,52 +1,5 @@
 var page = require('page');
-var yo = require('yo-yo');
-var empty = require('empty-element');
+require('./homepage');
+require('./signup');
 
-var main = document.getElementById('main-container');
-
-page('/', function (ctx, next) {
-  main.innerHTML = '<a href="/signup">Signup</a>'
-})
-
-page('/signup', function (ctx, next) {
-  var el = yo`<div class="container">
-      <div class="row">
-        <div class="col s10 push-s1">
-          <div class="row">
-            <div class="col m5 hide-on-small-only">
-              <img class="iphone" src="iphone.png" />
-            </div>
-            <div class="col s12 m7">
-              <div class="row">
-                <div class="signup-box">
-                  <h1 class="platzigram">MardwinGram</h1>
-                  <form class="signup-form">
-                    <h2>Regístrate para ver fotos de tus amigos</h2>
-                    <div class="section">
-                      <a class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
-                      <a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
-                    </div>
-                    <div class="divider"></div>
-                    <div class="section">
-                      <input type="email" name="email" placeholder="Correo electrónico" />
-                      <input type="text" name="name" placeholder="Nombre completo" />
-                      <input type="text" name="username" placeholder="Nombre de usuario" />
-                      <input type="password" name="password" placeholder="Contraseña" />
-                      <button class="btn waves-effect waves-light btn-signup" type="submit">Regístrate</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-              <div class="row">
-                <div class="login-box">
-                  ¿Tienes una cuenta? <a href="/signin">Entrar</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>`;
-    empty(main).appendChild(el);
-})
 page();
---
+++ src/signup/template.js
@@ -0,0 +1,40 @@
+var yo = require('yo-yo');
+
+module.exports = yo`<div class="container">
+      <div class="row">
+        <div class="col s10 push-s1">
+          <div class="row">
+            <div class="col m5 hide-on-small-only">
+              <img class="iphone" src="iphone.png" />
+            </div>
+            <div class="col s12 m7">
+              <div class="row">
+                <div class="signup-box">
+                  <h1 class="platzigram">MardwinGram</h1>
+                  <form class="signup-form">
+                    <h2>Regístrate para ver fotos de tus amigos</h2>
+                    <div class="section">
+                      <a class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
+                      <a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                      <input type="email" name="email" placeholder="Correo electrónico" />
+                      <input type="text" name="name" placeholder="Nombre completo" />
+                      <input type="text" name="username" placeholder="Nombre de usuario" />
+                      <input type="password" name="password" placeholder="Contraseña" />
+                      <button class="btn waves-effect waves-light btn-signup" type="submit">Regístrate</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="row">
+                <div class="login-box">
+                  ¿Tienes una cuenta? <a href="/signin">Entrar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
