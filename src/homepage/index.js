20c243325a104500af2408dc2d83243c16cb974a
Edwin <mardwintrader@gmail.com>
2016-11-16 10:41:09 -0400

    Commit 8

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,27 @@ var title = require('title');
 page('/', function (ctx, next) {
 	title('Mardwingram');
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+ var pictures = [
+    {
+      user: {
+        username: 'edwindeleong',
+        avatar: 'edwin.jpg'
+      },
+      url: 'office.jpg',
+      likes: 10,
+      liked: true
+    },
+    {
+      user: {
+        username: 'edwindeleong',
+        avatar: 'edwin.jpg'
+      },
+      url: 'office.jpg',
+      likes: 2,
+      liked: true
+    }
+  ];
+
+  empty(main).appendChild(template(pictures));
 })
