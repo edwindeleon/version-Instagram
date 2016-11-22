1aa11f58562501b12aa07195eda5f2cc1c3b41a5
Edwin <mardwintrader@gmail.com>
2016-11-22 19:07:30 -0400

    Commit 17

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require('express');
+var multer = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage }).single('picture');
 
 var app = express();
 
@@ -47,6 +60,14 @@ app.get('/api/pictures', function (req, res) {
 		res.send(pictures);
 	}, 2000)
 	
+});
+app.post('/api/pictures', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.send(500, "Error uploading file");
+    }
+    res.send('File uploaded');
+  })
 })
 
 app.listen(3000, function (err) {
